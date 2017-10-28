9ab8cb61e34bbdb9391885c4aefc3c034aaae4b6
jmrigali <jmrigali@ymail.com>
2017-10-28 14:11:30 -0600

    add pinterest sdk

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
- (()=>{
-   window.pAsyncInit = function() {
-       PDK.init({
-           appId: '4930913319035879965', // Change this
-           cookie: true
-       });
-   };
-
-   (function(d, s, id){
-       var js, pjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "//assets.pinterest.com/sdk/sdk.js";
-       pjs.parentNode.insertBefore(js, pjs);
-   }(document, 'script', 'pinterest-jssdk'));
- })()
+$(document).ready(function() {
+  let $xhr= $.getJSON('https://api.pinterest.com/v1/', (data)=>{
+    console.log(data)
+  })
+});
